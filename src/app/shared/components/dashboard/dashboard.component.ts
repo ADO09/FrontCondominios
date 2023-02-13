1a1881745cb25dfb1d12e855352657c8f3158e57
adrian-parra <adrian.adrianborquez@hotmail.com>
2023-02-12 17:46:39 -0700

    style dashboard
    Se agregaron los estilos generales para el dashboard

--- src/app/shared/components/dashboard/dashboard.component.ts
+++ src/app/shared/components/dashboard/dashboard.component.ts
@@ -1,15 +1,118 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { SharedTitleComponentService } from '../../../data/services/shared-title-component.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
+  tituloComponente!: string;
 
-  constructor() { }
+  menuDesplegado: Boolean = false;
 
-  ngOnInit(): void {
+  correo?:string
+  rol?:string
+
+  idEmpleado:string = ''
+  
+  
+  
+  constructor(private sharedTitleService: SharedTitleComponentService  ,private router:Router) {
+    sharedTitleService.changeEmitted$.subscribe((tituloComponente) => {
+      this.tituloComponente = tituloComponente;
+    });
   }
 
+  ngOnInit(): void {
+    
+  }
+
+  recetearContenedorEnlases() {
+    //VERIFICAR SI OTRO ELEMENTO ESTA ACTIVO Y DESACTIVARLO SI LO ESTA
+    let contenedoresEnlases = document.querySelectorAll('.contenedor-enlases');
+
+    contenedoresEnlases.forEach((elemento) => {
+      if (elemento.classList.contains('menu-item-flotante')) {
+        elemento.classList.remove('menu-item-flotante');
+      }
+      if (elemento.classList.contains('menu-item-espandible')) {
+        elemento.classList.remove('menu-item-espandible');
+      }
+    });
+  }
+
+  cerrarSesion(){
+    
+  }
+
+  abrirMenu() {
+    this.recetearContenedorEnlases();
+
+    this.menuDesplegado = this.menuDesplegado ? false : true;
+
+  }
+
+  espandirItemMenu(event: any): any {
+    var elemento = event.target as HTMLElement;
+
+
+    if(elemento.className == 'contenedor-perfil'){
+      elemento.classList.toggle('cardview-float-perfil-active')
+    }
+
+    if (elemento.tagName == 'A') {
+      this.recetearContenedorEnlases();
+
+      this.menuDesplegado = false;
+    }
+
+    if (elemento.id == 'eventClick') {
+      const contEnlales = elemento.parentNode?.querySelector(
+        '.contenedor-enlases'
+      );
+
+      if (!this.menuDesplegado) {
+        if (contEnlales?.classList.contains('menu-item-flotante')) {
+          contEnlales?.classList.toggle('menu-item-flotante');
+        } else {
+          //VERIFICAR SI OTRO ELEMENTO ESTA ACTIVO Y DESACTIVARLO SI LO ESTA
+          let contenedoresEnlases = document.querySelectorAll(
+            '.contenedor-enlases'
+          );
+
+          contenedoresEnlases.forEach((elemento) => {
+            if (elemento.classList.contains('menu-item-flotante')) {
+              elemento.classList.remove('menu-item-flotante');
+            }
+          });
+
+          contEnlales?.classList.toggle('menu-item-flotante');
+        }
+        return false;
+      }
+
+      if (contEnlales?.classList.contains('menu-item-espandible')) {
+        contEnlales?.classList.toggle('menu-item-espandible');
+      } else {
+        //VERIFICAR SI OTRO ELEMENTO ESTA ACTIVO Y DESACTIVARLO SI LO ESTA
+        let contenedoresEnlases = document.querySelectorAll(
+          '.contenedor-enlases'
+        );
+
+        contenedoresEnlases.forEach((elemento) => {
+          if (elemento.classList.contains('menu-item-espandible')) {
+            elemento.classList.remove('menu-item-espandible');
+          }
+        });
+
+        contEnlales?.classList.toggle('menu-item-espandible');
+      }
+    }
+  }
+  irRecibos(id:string,colaborador:string){
+      this.router.navigate(['/dashboard/report/average'] ,{queryParams:{id:id,colaborador:colaborador}})
+  }
+  
 }
