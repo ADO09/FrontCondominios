e865c9fec2220ef3f7b485154b6106818a7f4678
ADO09 <jai_car_or11@hotmail.com>
2023-02-24 13:11:36 -0600

    modal propiedaes

--- src/app/data/modules/usuarios/usuarios.module.ts
+++ src/app/data/modules/usuarios/usuarios.module.ts
@@ -3,20 +3,22 @@ import { CommonModule } from '@angular/common';
 import { CrudUsuariosComponent } from './crud-usuarios/crud-usuarios.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { RegsRoutingModule } from './usuarios-routing.module';
-import { ModalUsuariosComponent } from './modal-usuarios/modal-usuarios.component';
+
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [
     CrudUsuariosComponent,
-    ModalUsuariosComponent,
+    
     
   ],
   imports: [
     CommonModule,
     SharedModule,
-    RegsRoutingModule
+    RegsRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class UsuariosModule { }
--- src/app/shared/components/list/list-propiedades/list-propiedades.component.ts
+++ src/app/shared/components/list/list-propiedades/list-propiedades.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-list-propiedades',
@@ -6,10 +7,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-propiedades.component.scss']
 })
 export class ListPropiedadesComponent implements OnInit {
+ 
+  usuarios = [
+    {
+      id: 1,
+      nombre: 'Sebastian',
+      contacto: '6871203122',
+      propiedad: 'Casa N°6'
+    },
+    {
+      id: 2,
+      nombre: 'Roberto',
+      contacto: '668956325',
+      propiedad: 'Casa N°7'
+    }
+];
+  
+  showModal:boolean = false;
 
-  constructor() { }
+  usuarioModal = {
+    id: '',
+    nombre: '',
+    contacto: '',
+    propiedad: ''
+  }
+
+
+
+  form: any;
+  constructor(   private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+  
+    this.form = this.formBuilder.group({
+      id: '',
+      nombre: '',
+      contacto: '',
+      propiedad: ''
+    });
+  }
+
+  abrirModal(id: any, nombre: any, contacto: any, propiedad: any): void{
+    this.form = this.formBuilder.group({
+      id: id,
+      nombre: nombre,
+      contacto: contacto,
+      propiedad: propiedad
+    });
+  }
+ 
+
+  closeModal() {
+    const modal = document.getElementById('id01');
+  if (modal) {
+    modal.style.display = 'none';
+  }
+  }
+  enviarModal(){
+
   }
 
 }
