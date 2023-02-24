f116291ad28bcdf301b0927d95f5090264e58b9b
Sebastian V <sebastian.v@cogentibs.in>
2023-02-24 10:44:50 -0700

    modalUsuarios

--- src/app/data/modules/usuarios/crud-usuarios/crud-usuarios.component.ts
+++ src/app/data/modules/usuarios/crud-usuarios/crud-usuarios.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { UsuariosService } from 'src/app/data/services/usuarios/usuarios.service';
 
 @Component({
   selector: 'app-crud-usuarios',
@@ -6,10 +8,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crud-usuarios.component.css']
 })
 export class CrudUsuariosComponent implements OnInit {
+  usuarios = [
+    {
+      id: 1,
+      nombre: 'Sebastian',
+      contacto: '6871203122',
+      propiedad: 'Casa N°6'
+    },
+    {
+      id: 2,
+      nombre: 'Roberto',
+      contacto: '668956325',
+      propiedad: 'Casa N°7'
+    }
+];
+  
+  showModal:boolean = false;
 
-  constructor() { }
+  usuarioModal = {
+    id: '',
+    nombre: '',
+    contacto: '',
+    propiedad: ''
+  }
+
+
+
+  form: any;
+  constructor( private usuariosService: UsuariosService,  private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.usuariosService.$modal.subscribe((valor) =>{this.showModal = valor});
+    this.form = this.formBuilder.group({
+      id: '',
+      nombre: '',
+      contacto: '',
+      propiedad: ''
+    });
+  }
+
+  abrirModal(id: any, nombre: any, contacto: any, propiedad: any): void{
+    this.form = this.formBuilder.group({
+      id: id,
+      nombre: nombre,
+      contacto: contacto,
+      propiedad: propiedad
+    });
+  }
+
+  enviarModal(): void{
+    
   }
 
 }
--- src/app/data/modules/usuarios/usuarios.module.ts
+++ src/app/data/modules/usuarios/usuarios.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { CrudUsuariosComponent } from './crud-usuarios/crud-usuarios.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { RegsRoutingModule } from './usuarios-routing.module';
+import { ModalUsuariosComponent } from './modal-usuarios/modal-usuarios.component';
 
 
 
 @NgModule({
   declarations: [
     CrudUsuariosComponent,
+    ModalUsuariosComponent,
     
   ],
   imports: [
