b39db2020a2e4b6b8ac027866523619aba0b33ce
ADO09 <jai_car_or11@hotmail.com>
2023-02-25 20:05:51 -0600

    aaa

--- src/app/shared/components/modals/modal-list-propiedades/modal-list-propiedades.component.ts
+++ src/app/shared/components/modals/modal-list-propiedades/modal-list-propiedades.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-modal-list-propiedades',
@@ -7,9 +8,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModalListPropiedadesComponent implements OnInit {
 
-  constructor() { }
+  usuarios = [
+    {
+      id: 1,
+      nombre: 'Sebastian',
+      contacto: '6871203122',
+      propiedad: 'Casa N°6'
+    },
+    {
+      id: 2,
+      nombre: 'Roberto',
+      contacto: '668956325',
+      propiedad: 'Casa N°7'
+    }
+];
+  
+  showModal:boolean = false;
+
+  usuarioModal = {
+    id: '',
+    nombre: '',
+    contacto: '',
+    propiedad: ''
+  }
+
+
+
+  form: any;
+  constructor(   private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+  
+    this.form = this.formBuilder.group({
+      id: '',
+      nombre: '',
+      contacto: '',
+      propiedad: ''
+    });
+  }
+
+  abrirModal(id: any, nombre: any, contacto: any, propiedad: any): void{
+    this.form = this.formBuilder.group({
+      id: id,
+      nombre: nombre,
+      contacto: contacto,
+      propiedad: propiedad
+    });
+  }
+ 
+
+  closeModal() {
+    const modal = document.getElementById('id01');
+  if (modal) {
+    modal.style.display = 'none';
+  }
+  }
+  enviarModal(){
+
   }
 
 }
