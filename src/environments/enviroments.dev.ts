69eea54aba66d1148d8fac9f56c13678ccdca933
ADO09 <jai_car_or11@hotmail.com>
2023-03-23 15:33:49 -0600

    aaaaa

--- src/environments/enviroments.dev.ts
+++ src/environments/enviroments.dev.ts
@@ -1,5 +1,5 @@
 export const enviroment = {
     production:false,
-    //url: 'http://localhost:3000/api/',
-    urlAPI:'http://26.142.80.113:8000/api/v1/'
+    urlAPI: 'http://localhost:8000/api/v1/',
+    // urlAPI:'http://26.142.80.113:8000/api/v1/'
 }
