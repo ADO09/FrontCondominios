504637fcb5d78cf53b70465498889629b620694b
ADO09 <jai_car_or11@hotmail.com>
2023-04-24 00:16:16 -0600

    inq-prop

--- src/app/shared/components/modals/modal-list-inquilinos/modal-list-inquilinos.component.ts
+++ src/app/shared/components/modals/modal-list-inquilinos/modal-list-inquilinos.component.ts
@@ -12,7 +12,8 @@ export class ModalListInquilinosComponent {
   @Input() propietariosData!: propietarios[];
   @Input() cerrarModal: any;
   @Output() inquilinoSeleccionado = new EventEmitter<any>();
-
+  propietarioTemp:any[]=[]
+  public propietarios:any[]=[]
   p: number = 1;
   items = Array(150).fill(0).map((x, i) => `Item ${i + 1}`);
 
@@ -21,5 +22,22 @@ export class ModalListInquilinosComponent {
     // const datoString = JSON.stringify(data);
  this.inquilinoSeleccionado.emit(data);
     this.cerrarModal();
+  }
+
+
+  searchPropietario(search:any ,event:any){
+
+    this.propietarios = this.propietariosData;
+    event.stopPropagation();
+    //event.stopPropagation();
+    //console.log(search)
+    const matchingPropietario = this.propietarioTemp.filter(u => 
+      u.descripcion.toLowerCase().includes(search.toLowerCase()) || 
+      u.cantidad.toLowerCase().includes(search.toLowerCase()) || 
+      u.precio_unitario.toLowerCase().includes(search.toLowerCase())
+    );
+    //console.log(matchingUsers);
+
+    this.propietarios = matchingPropietario
   }
 }
--- src/app/shared/components/modals/modal-list-propietarios/modal-list-propietarios.component.ts
+++ src/app/shared/components/modals/modal-list-propietarios/modal-list-propietarios.component.ts
@@ -9,18 +9,40 @@ import { propietarios } from 'src/app/data/interfaces/propietariosI';
 export class ModalListPropietariosComponent {
   @Input() propietariosData!: propietarios[];
   @Input() cerrarModal: any;
+  propietarioTemp:any[]=[]
+  public propietarios:any[]=[]
   @Output() inquilinoSeleccionado = new EventEmitter<any>();
   p: number = 1;
   items = Array(150).fill(0).map((x, i) => `Item ${i + 1}`);
 
 
-  selectInquilino(data:any){
+  selectpropietarios(data:any){
    // console.log(data);
     // const datoString = JSON.stringify(data);
  this.inquilinoSeleccionado.emit(data);
     this.cerrarModal();
   }
 
+
+  searchPropietario(search:any ,event:any){
+
+    this.propietarios = this.propietariosData;
+    event.stopPropagation();
+    console.log('iaushdiuahsdiuashiudhasiudhaiu');
+    
+    console.log(this.propietarios);
+    
+    //event.stopPropagation();
+    //console.log(search)
+    const matchingPropietario = this.propietarioTemp.filter(u => 
+      u.nombre.toLowerCase().includes(search.toLowerCase()) || 
+      u.id.toLowerCase().includes(search.toLowerCase()) || 
+      u.correo.toLowerCase().includes(search.toLowerCase())
+    );
+    //console.log(matchingUsers);
+
+    this.propietarios = matchingPropietario
+  }
 
 
  
