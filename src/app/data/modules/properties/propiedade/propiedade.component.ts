0b3b8fba4565cb4bbf5c0553fb0c684e7fa28bd6
ADO09 <jai_car_or11@hotmail.com>
2023-06-11 21:40:24 -0600

    as

--- src/app/data/modules/properties/propiedade/propiedade.component.ts
+++ src/app/data/modules/properties/propiedade/propiedade.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PropiedadesServiceService } from 'src/app/data/services/api/propiedades/propiedades-service.service';
+import { animacionSearch } from '../../../../shared/exports/animacionInputSearch';
+import { INTERNAL_ROUTES } from 'src/app/data/constants/routes/internal.routes';
 
 @Component({
   selector: 'app-propiedade',
@@ -8,20 +10,58 @@ import { PropiedadesServiceService } from 'src/app/data/services/api/propiedades
 })
 export class PropiedadeComponent implements OnInit {
   public idFraccionamientoUsuer:any;
- 
-  constructor(private propiedadesService:PropiedadesServiceService) { }
+  public regPropiedadRoute:any;
+  constructor(private propiedadesService:PropiedadesServiceService) { 
+    this.regPropiedadRoute =  INTERNAL_ROUTES.MODULO_REGPROPIEDAD;
+
+  }
 
   public propiedadesData!:any[];
+  propiedadesTemp!:any[]
   ngOnInit(): void {
     this.idFraccionamientoUsuer = localStorage.getItem('id_fraccionamiento');
     // console.log(this.idFraccionamientoUsuer);
     this.propiedadesService.propiedadesGetFiltroFraccionamiento(this.idFraccionamientoUsuer).subscribe( (r) => {
 
       this.propiedadesData = r.body;
+      this.propiedadesTemp = r.body;
       // console.log(r);
       // console.log(this.propiedadesData);
       
     });
+  }
+
+
+
+  
+  animacionSearch(){
+    animacionSearch.animacionSearch()
+  }
+  searchVehiculo(search:any ,event:any){
+    const matchingPropiedades = this.propiedadesTemp.filter((u:any) => 
+    u.descripcion.toLowerCase().includes(search.toLowerCase()) || 
+    u.propietario.nombre.toLowerCase().includes(search.toLowerCase()));
+  //console.log(matchingUsers);
+
+    this.propiedadesData = matchingPropiedades
+  }
+
+
+  getPropiedadesFilters(event:Event){
+    const selectedValue = (event.target as HTMLSelectElement).value;
+
+    var valuesSelectTipoPropiedad = ''
+    if(selectedValue != '-1')valuesSelectTipoPropiedad = selectedValue
+
+    this.propiedadesService.getPropiedesFilters(valuesSelectTipoPropiedad).subscribe((data:any)=>{
+      this.propiedadesData = data.body;
+      this.propiedadesTemp = data.body;
+    })
+
+  }
+
+  cerrarAnimacioneSearch(){
+    animacionSearch.cerrarAnimacioneSearch()
   }
 
 }
