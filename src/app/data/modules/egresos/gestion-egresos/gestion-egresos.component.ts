c7ffaf2f6fc2f54cfbc1b534562213e625379c22
ADO09 <jai_car_or11@hotmail.com>
2023-06-19 16:31:35 -0600

    si

--- src/app/data/modules/egresos/gestion-egresos/gestion-egresos.component.ts
+++ src/app/data/modules/egresos/gestion-egresos/gestion-egresos.component.ts
@@ -4,32 +4,45 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Egreso } from 'src/app/data/interfaces/egresos';
 import { EgresosService } from 'src/app/data/services/api/egresos/egresos.service';
 import { ProveedoresService } from 'src/app/data/services/api/proveedores/proveedores.service';
+import { UpdateDataService } from 'src/app/data/services/update-data.service';
 import { enviroment as ENV } from 'src/environments/enviroments.dev';
 
 @Component({
   selector: 'app-gestion-egresos',
   templateUrl: './gestion-egresos.component.html',
-  styleUrls: ['./gestion-egresos.component.css']
+  styleUrls: ['./gestion-egresos.component.css'],
 })
 export class GestionEgresosComponent {
   public FormEgresos: any;
-  public egresoData!:any;
- public id:any;
- public tiposEgresos!:any;
- public formData = new FormData();
- public selectedFile: File | undefined;
- public  pdfUrl: any;
- public api = ENV.urlAPI;
- showModal = false
- productoTemp:any[]=[]
- proveedores:any[]=[]
- idProveedor: string | undefined;
- correoProveedor: string | undefined;
- public proveedorSelect: any | null = null;
+  public egresoData!: any;
+  public id: any;
+  public tiposEgresos!: any;
+  public formData = new FormData();
+  public selectedFile: File | undefined;
+  public pdfUrl: any;
+  public api = ENV.urlAPI;
+  showModal = false;
+  productoTemp: any[] = [];
+  proveedores: any[] = [];
+  idProveedor: string | undefined;
+  correoProveedor: string | undefined;
+  public proveedorSelect: any | null = null;
   listaEgresos: any;
 
-
-  constructor(private proveedoresService:ProveedoresService, private route: ActivatedRoute,private egresosService:EgresosService,private formBuilder:FormBuilder,private router:Router){
+  constructor(
+    private proveedoresService: ProveedoresService,
+    private route: ActivatedRoute,
+    private egresosService: EgresosService,
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private updateDService:UpdateDataService
+  ) {
+    updateDService.changeEmitted$.subscribe((data) => {
+      // this.tipoEgresoListUpdate();
+      this.egresosService.getOneEgreso(this.id).subscribe((r) => {
+        this.egresoData = r.body;
+      });
+    });
     this.id = +this.route.snapshot.params['id_egreso'];
     console.log(this.id);
     this.FormEgresos = this.formBuilder.group({
@@ -39,100 +52,99 @@ export class GestionEgresosComponent {
       comprobanteUrl: [, Validators.required],
       montoTotal: ['', Validators.required],
       isVerified: ['', Validators.required],
-      tipoEgreso:['', Validators.required] ,
-      estatusEgreso:['', Validators.required] ,
+      tipoEgreso: ['', Validators.required],
+      estatusEgreso: ['', Validators.required],
       tipoPago: ['', Validators.required],
-      fechaPago:['', Validators.required] ,
-      proveedorId:['', Validators.required] ,
+      fechaPago: ['', Validators.required],
+      proveedorId: ['', Validators.required],
       // TipoEgresoEstatus:['', Validators.required]
       // detalleEgreso: ['', Validators.required]
     });
   }
 
+  async ngOnInit() {
+    var data = (await this.proveedoresService.getAll().toPromise()) as any;
+    this.proveedores = data.body;
 
-  async ngOnInit(){
-
-    var data  = (await this.proveedoresService.getAll().toPromise()) as any;
-    this.proveedores = data.body
-    
-
-
-   
     this.egresosService.getOneEgreso(this.id).subscribe((r) => {
       this.egresoData = r.body;
       console.log(this.egresoData);
-      
- this.egresosService.getTipoEgresoQPFraccionamiento(localStorage.getItem('id_fraccionamiento')).subscribe((res) =>{
-      
-  
-  this.tiposEgresos = res.body;
-  
-  console.log(this.tiposEgresos);
-  
-       
-     
-        this.FormEgresos = this.formBuilder.group({
-          descripcion: [this.egresoData.descripcion, Validators.required],
-          comprobanteUrl: [''],
-          montoTotal: [this.egresoData.montoTotal, Validators.required],
-          isVerified: [this.egresoData.isVerified, Validators.required],
-          tipoEgreso: [this.egresoData.tipoEgreso.id, Validators.required],
-          estatusEgreso: [this.egresoData.estatusEgreso.id, Validators.required],
-          tipoPago: [this.egresoData.tipoPago, Validators.required],
-          fechaPago:[this.egresoData.fechaPago, Validators.required] ,
-          proveedorId:[this.egresoData.proveedor.correoContacto, Validators.required] ,
-          // TipoEgresoEstatus:[this.egresoData.tipoEgreso.status, Validators.required]
-        });
-       
 
-      });
-    
-         console.log(this.egresoData.detalleEgreso);
-        
-    });  
+      this.egresosService
+        .getTipoEgresoQPFraccionamiento(
+          localStorage.getItem('id_fraccionamiento')
+        )
+        .subscribe((res) => {
+          this.tiposEgresos = res.body;
+
+          console.log(this.tiposEgresos);
+
+          this.FormEgresos = this.formBuilder.group({
+            descripcion: [this.egresoData.descripcion, Validators.required],
+            comprobanteUrl: [''],
+            montoTotal: [this.egresoData.montoTotal, Validators.required],
+            isVerified: [this.egresoData.isVerified, Validators.required],
+            tipoEgreso: [this.egresoData.tipoEgreso.id, Validators.required],
+            estatusEgreso: [
+              this.egresoData.estatusEgreso.id,
+              Validators.required,
+            ],
+            tipoPago: [this.egresoData.tipoPago, Validators.required],
+            fechaPago: [this.egresoData.fechaPago, Validators.required],
+            proveedorId: [
+              this.egresoData.proveedor.correoContacto,
+              Validators.required,
+            ],
+            // TipoEgresoEstatus:[this.egresoData.tipoEgreso.status, Validators.required]
+          });
+        });
+
+      console.log(this.egresoData.detalleEgreso);
+    });
   }
 
-
-  
   fraccionamientoId(fraccionamientoId: any) {
     throw new Error('Method not implemented.');
   }
 
-  enviarModal(){
-
+  enviarModal() {
     console.log(this.FormEgresos.value);
-  
 
-    var TipoEgreso:any =  this.FormEgresos.value.tipoEgreso;
+    var TipoEgreso: any = this.FormEgresos.value.tipoEgreso;
 
-    var fraccionamientoId:any = localStorage.getItem('id_fraccionamiento');
-   this.formData.append('descripcion', this.FormEgresos.value.descripcion);
-    this.formData.append('comprobanteUrl', this.FormEgresos.value.comprobanteUrl);
+    var fraccionamientoId: any = localStorage.getItem('id_fraccionamiento');
+    this.formData.append('descripcion', this.FormEgresos.value.descripcion);
+    this.formData.append(
+      'comprobanteUrl',
+      this.FormEgresos.value.comprobanteUrl
+    );
     this.formData.append('montoTotal', this.FormEgresos.value.montoTotal);
     this.formData.append('isVerified', this.FormEgresos.value.isVerified);
-     this.formData.append('tipoEgreso', TipoEgreso);
+    this.formData.append('tipoEgreso', TipoEgreso);
     // this.formData.append('estatusEgreso', this.FormEgresos.value.estatusEgreso);
-     this.formData.append('estatusEgresoId', this.FormEgresos.value.estatusEgreso);
+    this.formData.append(
+      'estatusEgresoId',
+      this.FormEgresos.value.estatusEgreso
+    );
     this.formData.append('fraccionamientoId', fraccionamientoId);
-      this.formData.append('fechaPago', this.FormEgresos.value.fechaPago);
+    this.formData.append('fechaPago', this.FormEgresos.value.fechaPago);
     this.formData.append('tipoPago', this.FormEgresos.value.tipoPago);
 
+    this.egresosService
+      .updateEgreso(this.egresoData.id, this.formData)
+      .subscribe((r) => {
+        console.log(r);
 
-    this.egresosService.updateEgreso(this.egresoData.id,this.formData).subscribe( (r)=>{
-      console.log(r);
-      
-      if (r.icon=='success') {
-        this.router.navigateByUrl('/dashboard/egresos')
-      }
-    })
-    
+        if (r.icon == 'success') {
+          this.router.navigateByUrl('/dashboard/egresos');
+        }
+      });
   }
-
 
   onFileSelected(event: any) {
     this.selectedFile = event.target.files[0];
     this.FormEgresos.value.comprobanteUrl = event.target.files[0];
-   
+
     const file = event.target.files[0];
     this.formData.append('archivoComprobante', file);
     const reader = new FileReader();
@@ -145,106 +157,88 @@ export class GestionEgresosComponent {
     (document.querySelector('#inputFile') as HTMLInputElement).click();
   }
 
-  previewFile() {
-    
-  }
+  previewFile() {}
 
-
-
-  
-  searchProveedor(search:any ,event:any){
-
+  searchProveedor(search: any, event: any) {
     event.stopPropagation();
     //event.stopPropagation();
     //console.log(search)
-    const matchingProducto = this.productoTemp.filter(u => 
-      u.descripcion.toLowerCase().includes(search.toLowerCase()) || 
-      u.cantidad.toLowerCase().includes(search.toLowerCase()) || 
-      u.precio_unitario.toLowerCase().includes(search.toLowerCase())
+    const matchingProducto = this.productoTemp.filter(
+      (u) =>
+        u.descripcion.toLowerCase().includes(search.toLowerCase()) ||
+        u.cantidad.toLowerCase().includes(search.toLowerCase()) ||
+        u.precio_unitario.toLowerCase().includes(search.toLowerCase())
     );
     //console.log(matchingUsers);
 
-    this.proveedores = matchingProducto
+    this.proveedores = matchingProducto;
   }
 
-  selectProveedor(proveedor:any){
+  selectProveedor(proveedor: any) {
+    console.log('2');
 
-    
-   
-      console.log('2');
-      
-      this.idProveedor = String(proveedor.id);
+    this.idProveedor = String(proveedor.id);
 
     console.log(proveedor.id);
-    
+
     this.correoProveedor = String(proveedor.correoContacto);
 
     this.proveedorSelect = proveedor;
     console.log(this.proveedorSelect?.correoContacto);
     this.FormEgresos.patchValue({
-      proveedorId: [
-        this.proveedorSelect?.correoContacto 
-      ],
+      proveedorId: [this.proveedorSelect?.correoContacto],
     });
 
     // this.formData.append('productoId', producto.id);
 
     this.formData.append('proveedorId', proveedor.id);
-    this.showModal = false
-    
-  
+    this.showModal = false;
   }
 
-
-  tipoEgresoSeleccionado(select:any){
-   
+  tipoEgresoSeleccionado(select: any) {
     const selectedIndex = select.selectedIndex;
-  const selectedObject:any = this.tiposEgresos[selectedIndex];
-  console.log(selectedObject.proveedorDefault);
+    const selectedObject: any = this.tiposEgresos[selectedIndex];
+    console.log(selectedObject.proveedorDefault);
 
-  if (selectedObject.proveedorDefault) {
-    console.log('aisudhashudiu');
-    
-    this.idProveedor = String(selectedObject.proveedorDefault.id);
+    if (selectedObject.proveedorDefault) {
+      console.log('aisudhashudiu');
 
-    console.log(selectedObject.proveedorDefault.id);
-    
-    this.correoProveedor = String(selectedObject.proveedorDefault.correoContacto);
+      this.idProveedor = String(selectedObject.proveedorDefault.id);
 
-    this.proveedorSelect = selectedObject.proveedorDefault;
-    console.log(this.proveedorSelect?.correoContacto);
-    this.FormEgresos.patchValue({
-      proveedorId: [
-        this.proveedorSelect?.correoContacto 
-      ],
-    });
+      console.log(selectedObject.proveedorDefault.id);
 
-    // this.formData.append('productoId', producto.id);
+      this.correoProveedor = String(
+        selectedObject.proveedorDefault.correoContacto
+      );
 
-    this.formData.append('proveedorId', selectedObject.proveedorDefault.id);
-    // this.showModal = false
-  }else{
-   
-    //console.log(this.proveedorSelect?.correoContacto);
-    this.idProveedor = String(this.egresoData.proveedor.id);
+      this.proveedorSelect = selectedObject.proveedorDefault;
+      console.log(this.proveedorSelect?.correoContacto);
+      this.FormEgresos.patchValue({
+        proveedorId: [this.proveedorSelect?.correoContacto],
+      });
 
-    console.log(this.egresoData.proveedor.id);
-    
-    this.correoProveedor = String(this.egresoData.proveedor.correoContacto);
+      // this.formData.append('productoId', producto.id);
 
-    this.proveedorSelect = this.egresoData.proveedor;
-    console.log(this.proveedorSelect?.correoContacto);
-    this.FormEgresos.patchValue({
-      proveedorId: [
-        this.proveedorSelect?.correoContacto 
-      ],
-    });
+      this.formData.append('proveedorId', selectedObject.proveedorDefault.id);
+      // this.showModal = false
+    } else {
+      //console.log(this.proveedorSelect?.correoContacto);
+      this.idProveedor = String(this.egresoData.proveedor.id);
 
-    // this.formData.append('productoId', producto.id);
+      console.log(this.egresoData.proveedor.id);
 
-    this.formData.append('proveedorId', this.egresoData.proveedor.id);
-    //this.showModal = false
+      this.correoProveedor = String(this.egresoData.proveedor.correoContacto);
+
+      this.proveedorSelect = this.egresoData.proveedor;
+      console.log(this.proveedorSelect?.correoContacto);
+      this.FormEgresos.patchValue({
+        proveedorId: [this.proveedorSelect?.correoContacto],
+      });
+
+      // this.formData.append('productoId', producto.id);
+
+      this.formData.append('proveedorId', this.egresoData.proveedor.id);
+      //this.showModal = false
+    }
   }
-  }
-
 }
--- src/app/shared/components/list/list-detalles-egresos/list-detalles-egresos.component.ts
+++ src/app/shared/components/list/list-detalles-egresos/list-detalles-egresos.component.ts
@@ -3,24 +3,29 @@ import { ActivatedRoute, RouterLinkActive } from '@angular/router';
 import { INTERNAL_ROUTES } from 'src/app/data/constants/routes/internal.routes';
 import { EgresosService } from 'src/app/data/services/api/egresos/egresos.service';
 import { SharedTitleComponentService } from 'src/app/data/services/shared-title-component.service';
+import { UpdateDataService } from 'src/app/data/services/update-data.service';
 import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-list-detalles-egresos',
   templateUrl: './list-detalles-egresos.component.html',
-  styleUrls: ['./list-detalles-egresos.component.css']
+  styleUrls: ['./list-detalles-egresos.component.css'],
 })
 export class ListDetallesEgresosComponent {
-  
   @Input() dataDetallesEgreso!: any[];
-  public currentDetalleEgreso!:any;
-  public id:any;
-  public routeDetalleRg:any = INTERNAL_ROUTES.MODULO_REGEDETALLEEGRESO;
-  constructor(private routerA:ActivatedRoute,private egresoService:EgresosService,private sharedTitleService:SharedTitleComponentService){
+  public currentDetalleEgreso!: any;
+  public id: any;
+  public routeDetalleRg: any = INTERNAL_ROUTES.MODULO_REGEDETALLEEGRESO;
+  constructor(
+    private routerA: ActivatedRoute,
+    private egresoService: EgresosService,
+    private sharedTitleService: SharedTitleComponentService,
+    private updateDService:UpdateDataService
+  ) {
     this.id = +this.routerA.snapshot.params['id_egreso'];
-    sharedTitleService.emitChange("Gestion de Egresos")
+    sharedTitleService.emitChange('Gestion de Egresos');
   }
-  abrirModal(data:any){
+  abrirModal(data: any) {
     setTimeout(() => {
       var divModl = document.getElementById('id01') as HTMLDivElement;
       divModl.style.display = 'block';
@@ -43,7 +48,7 @@ export class ListDetallesEgresosComponent {
       showCancelButton: true,
       confirmButtonColor: '#3085d6',
       cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes, delete it!'
+      confirmButtonText: 'Yes, delete it!',
     }).then((result) => {
       if (result.isConfirmed) {
         // Swal.fire(
@@ -51,18 +56,18 @@ export class ListDetallesEgresosComponent {
         //   'Your file has been deleted.',
         //   'success'
         // )
-   
 
-        this.egresoService.DelDetalleEgreso(this.id,detalleEgreso.producto.id).subscribe((r)=>{
-          console.log(r);
-          
-        })
+        this.egresoService
+          .DelDetalleEgreso(this.id, detalleEgreso.producto.id)
+          .subscribe((r) => {
+            console.log(r);
+            if (r.icon == 'success') {
+              this.updateDService.emitChange('hola');
+            }
+          });
       }
-    })
+    });
 
-  
     // Agrega aquí la lógica para eliminar el detalle de egreso seleccionado
   }
-  
-  
 }
